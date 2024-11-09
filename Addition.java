5bca56ff2dbe014a1f66aaf7662fcd3c3bfe98e8
Mouni2695 <kalimounika02@gmail.com>
2024-11-09 11:18:05 +0530

    Adding the msg

---
+++ Addition.java
@@ -0,0 +1,10 @@
+class Addition
+{
+  public static void main(String args[])
+  {
+    //Additon
+    int a=2,b=3,c;
+    c=a+b;
+    System.out.println(c);
+  }
+}
---
+++ Subtraction.java
@@ -0,0 +1,9 @@
+class Subtraction
+{
+  public static void main(String args[])
+  {
+    int a1=6,a2=3,a3;
+    a3=a1-a2;
+    System.out.println(a3);
+  }
+}
